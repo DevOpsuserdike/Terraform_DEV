f4ee9a7989606f00a42dd599065a20bffb55bca2
DevOpsuserdike <devopssiddhesh@gmail.com>
2026-07-10 20:39:48 +0530

    u[date

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-052cab3d74c050516" # Ubuntu Server 24.04 LTS (ap-south-1)
+  ami           = "ami-002192a70217ac181" # Ubuntu Server 24.04 LTS (ap-south-1)
   instance_type = var.instance_type
 }
 
