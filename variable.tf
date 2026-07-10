827c9f0af1d51352d54ae4192de7825c4811e3df
DevOpsuserdike <devopssiddhesh@gmail.com>
2026-07-10 20:04:00 +0530

    u[date

--- variable.tf
+++ variable.tf
@@ -1,22 +1,5 @@
-variable "aws_region" {
-  description = "The AWS region to deploy into"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
-}
-
-variable "instance_name" {
-  description = "Name tag for the EC2 instance"
-  type        = string
-}
-
-
-variable "name" {
-  description = "Name tag"
-  type        = string
 }
