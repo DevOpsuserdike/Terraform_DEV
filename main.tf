7a473b5956c502bdd1b3c56fa5fdaf6554900277
DevOpsuserdike <devopssiddhesh@gmail.com>
2026-07-10 21:19:44 +0530

    u[date

--- main.tf
+++ main.tf
@@ -3,3 +3,30 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
 }
 
+
+data "aws_s3_bucket" "selected" {
+  bucket = "s320260710"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "lifecyclerule" {
+  bucket = data.aws_s3_bucket.selected.id
+
+  rule {
+    id = "Allow small object transitions"
+
+    filter {
+      prefix                   = "logs/"
+    }
+
+    status = "Enabled"
+
+    transition {
+      days          = 2
+      storage_class = "GLACIER_IR"
+    }
+
+     expiration {
+      days = 3
+    }
+  }
+}
